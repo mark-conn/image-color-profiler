f200b671470d4dbcfee822ad1ec657f98962e298
Nathaniel Kitzke <nkitzke@shutterstock.com>
2019-04-04 13:40:46 -0400

    indexer setup for list of ids and shared cluster

--- app/indexer/index.js
+++ app/indexer/index.js
@@ -3,19 +3,27 @@ const getColors = require("get-image-colors");
 const fs = require("fs");
 const { api } = require("../server/search");
 const { getColorBucket } = require("../utils");
+const imageIds = require('../images/ids.json')
+
+const cdnUrl = 'https://image.shutterstock.com/z/'
+
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
 
 // Read images from folder 1 at a time and index to elastic via api.
-fs.readdirSync(path.join(__dirname, "../images/")).forEach(async image => {
+asyncForEach(imageIds, async (imageId) => {
+  const imageSource = cdnUrl+imageId
+
   // get dominant colors from image
-  const domninantColors = await getColors(path.join(__dirname, "../images/", image));
+  const domninantColors = await getColors(imageSource);
 
   // get color buckets for each dominant color
   const colorBuckets = domninantColors.map(c => getColorBucket(c._rgb.slice(0, 3)));
 
   // Index the picture
-  const result = await api.indexDocument({ colorProfile: colorBuckets, source: source });
-
-  console.log("\n\n\n\n\n result \n", result, "\n\n\n\n\n");
-
-  // extract elastic ID for image and store image using this value.
-});
+  api.indexDocument({ colorProfile: colorBuckets, source: imageSource });
+  console.log('\n', imageSource, '   ', colorBuckets, '\n')
+})
--- app/server/search.js
+++ app/server/search.js
@@ -1,7 +1,7 @@
 const { Client } = require('@elastic/elasticsearch');
 // const request = require('request');
 
-const client = new Client({ node: 'http://localhost:9200' });
+const client = new Client({ node: 'http://10.128.49.81:9200' });
 
 const api = {
   indexDocument: async ({colorProfile, source}) => {
